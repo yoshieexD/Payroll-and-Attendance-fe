4993dd267b3136930fd62cb7d4c6a103beec63a4
Jom Winston Magbag <bjomwinston0107@gmail.com>
2024-02-07 17:36:28 +0800

    [ADD] delete

--- client/src/table/EmployeeTable.tsx
+++ client/src/table/EmployeeTable.tsx
@@ -1,9 +1,25 @@
 import { IEmployee } from '../interface/Interface'
 import useGet from "../api/useGet";
+import { useState } from 'react';
+import useDelete from '../api/useDelete';
+import { toast } from 'react-hot-toast';
+
 
 
 function EmployeeTable() {
     const { data } = useGet({ getQuery: 'get-employee', url: '/employee/get' })
+    const [ids, setIds] = useState('');
+    const [open, setOpen] = useState('');
+
+    const deletes: any = useDelete({
+        url: `/employee/delete/${ids}`,
+        onSuccess: () => {
+            toast.success('Successfully deleted employee');
+        },
+        query: 'get-employee',
+        errors: 'You don\'t have admin privileges',
+    });
+
     return (
         <div className="overflow-x-auto w-full">
             <table className="table">
@@ -26,14 +42,14 @@ function EmployeeTable() {
                                     <td>{e.name}</td>
                                     <td>{e.employmentType}</td>
                                     <td className='space-x-2'>
-                                        <button className="btn btn-accent">View</button>
-                                        <button className="btn btn-error">Delete</button>
+                                        <button className="btn btn-accent" onClick={async () => { await setIds(e._id); setOpen('open') }}>View</button>
+                                        <button className="btn btn-error" onClick={async () => { await setIds(e._id); deletes.mutate(); }}>Delete</button>
                                     </td>
                                 </tr>
                             ))
                         ) : (
                             <tr>
-                                <td colSpan={4}> No data found</td>
+                                <td colSpan={4} className='text-center'> No data found</td>
                             </tr>
                         )
                     }
